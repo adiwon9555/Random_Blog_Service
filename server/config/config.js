824cfb2dbe5b8037a77eacfead6b936bab7e7387
adiwon9555 <aditya.rathi9555@gmail.com>
2018-12-06 19:11:22 +0530

    Port made to 80

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@
 var env=process.env.NODE_ENV || 'development';
 if(env === 'test')
 {
-    process.env.PORT=3000;
+    process.env.PORT=80;
     process.env.MONGODB_URI='mongodb://localhost:27017/BlogTestApp'
 }else if(env === 'development')
 {
-    process.env.PORT=3000;
+    process.env.PORT=80;
     process.env.MONGODB_URI='mongodb://localhost:27017/BlogApp'
 }
